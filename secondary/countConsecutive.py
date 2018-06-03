825e9621fa12639644bb2c669fcbf95c6d4253f1
Daniel Semeniuta <dsemeniu@stanford.edu>
2018-06-03 09:54:45 -0700

    counting zeros in MTF pre and post transform

--- secondary/countConsecutive.py
+++ secondary/countConsecutive.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 '''
 Naive implementation of BWT taken from Wikipedia
@@ -28,21 +29,51 @@ def countConsecutive(s, graphname, imgname):
         else:
             counts[count] += 1
             count = 1
-    plt.bar(range(len(counts)), counts)
+    plt.bar(range(len(counts)), counts, log=True)
     plt.xlabel('Length of run of consecutive characters')
     plt.ylabel('Number of (log) occurences')
-    plt.yscale('log')
     plt.title(graphname)
     plt.savefig(imgname + '.png')
     plt.clf()
+
+
+def moveToFront(s):
+    Y = sorted(list(set(s)))
+    Y_sort = Y
+    codedText = list()
+    rank = 0
+    for c in s:
+        rank = Y.index(str(c))
+        codedText += [rank]
+        Y.pop(rank)
+        Y.insert(0, c)
+
+    Y = Y_sort
+    return codedText, Y
+
+def normOfMoveToFront(s):
+    code, Y = moveToFront(s)
+    code = np.array(code)
+    norm = np.linalg.norm(code)
+    num_zeros = code.size - np.count_nonzero(code)
+    percent_zero = 1.0 * num_zeros / code.size
+    return norm, percent_zero
 
 def main():
     for title, text in [('Green Eggs and Ham', 'green_eggs_and_ham'), ('Constitution', 'const')]:
         filename = './sample_texts/' + text + '.txt'
         S = loadText(filename)
         countConsecutive(S, '%s pre-transform' % title, '%s_pre' % text)
+        pre_norm, pre_percent_zero = normOfMoveToFront(S)
         L = bwt(S)
         countConsecutive(L, '%s post-transform' % title, '%s_post' % text)
+        post_norm, post_percent_zero = normOfMoveToFront(L)
+        print '%s stats:' % title
+        print 'Norm differences:'
+        print '%f to %f, ratio of %f' % (pre_norm, post_norm, 1.0*pre_norm / post_norm)
+        print 'Percent Zero differences:'
+        print '%f to %f' % (pre_percent_zero, post_percent_zero)
+        print '\n'
 
 
 if __name__ == '__main__':
